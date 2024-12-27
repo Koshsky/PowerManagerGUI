8fcd0a25baf7e12d6fa9d915baba27f1edff073f
Shmonov Matvey Vasilievich <meology77@gmail.com>
2024-12-27 17:48:42 +0300

    replace my ping-method with nmap from github library

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require fyne.io/fyne/v2 v2.5.2 // direct
 require (
 	fyne.io/systray v1.11.0 // indirect
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/Ullaakut/nmap/v2 v2.2.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fredbi/uri v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/jeandeaual/go-locale v0.0.0-20240223122105-ce5225dcaa49 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rymdport/portal v0.2.6 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
@@ -31,6 +33,7 @@ require (
 	golang.org/x/image v0.18.0 // indirect
 	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
 	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/netutils/netutils.go
+++ internal/netutils/netutils.go
@@ -2,116 +2,57 @@ package netutils
 
 import (
 	"fmt"
-	"net"
 	"strconv"
-	"sync"
 	"time"
+
+	nmap "github.com/Ullaakut/nmap/v2"
 )
 
-func IsValidOctet(input string) bool {
+func ScanNetwork(operatingRoom string) ([]string, error) {
+	if !isValidOctet(operatingRoom) {
+		return []string{}, fmt.Errorf("ScanNetwork: incorrect operating room number [1-255]: %s", operatingRoom)
+	}
+
+	roomNum, err := strconv.Atoi(operatingRoom)
+	if err != nil {
+		return []string{}, fmt.Errorf("ScanNetwork: %w", err)
+	} else if roomNum < 1 || roomNum > 255 {
+		return []string{}, fmt.Errorf("ScanNetwork: the operating room number is outside the range 1-255")
+	}
+
+	subnet := fmt.Sprintf("10.4.%d.0/24", roomNum)
+	return scanSubnet(subnet)
+}
+
+func scanSubnet(subnet string) ([]string, error) {
+	scanner, err := nmap.NewScanner(
+		nmap.WithTargets(subnet),
+		nmap.WithPingScan(),
+		nmap.WithHostTimeout(1*time.Second), // Устанавливаем таймаут в 1 секунду
+	)
+	if err != nil {
+		return []string{}, fmt.Errorf("error creating scanner: %v", err)
+	}
+
+	result, _, err := scanner.Run()
+	if err != nil {
+		return []string{}, fmt.Errorf("scanning error: %v", err)
+	}
+	print(result)
+
+	var allReachableIPs []string
+	for _, host := range result.Hosts {
+		if host.Status.State == "up" {
+			allReachableIPs = append(allReachableIPs, host.Addresses[0].Addr)
+		}
+	}
+	return allReachableIPs, nil
+}
+
+func isValidOctet(input string) bool {
 	octet, err := strconv.Atoi(input)
 	if err != nil {
 		return false
 	}
 	return octet >= 1 && octet <= 255
-}
-
-func ScanNetwork(operatingRoom string) ([]string, error) {
-	if !IsValidOctet(operatingRoom) {
-		return []string{}, fmt.Errorf("ScanNetwork: incorrect operating room number [1-255]: %s", operatingRoom)
-	}
-
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	var allReachableIPs []string
-
-	roomNum, err := strconv.Atoi(operatingRoom)
-	if err != nil {
-		return allReachableIPs, fmt.Errorf("ScanNetwork: %w", err)
-	} else if roomNum < 1 || roomNum > 255 {
-		return allReachableIPs, fmt.Errorf("ScanNetwork: the operating room number is outside the range 1-255")
-	}
-
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		gersIPs, _ := scanGERSManagers(operatingRoom)
-		mu.Lock()
-		allReachableIPs = append(allReachableIPs, gersIPs...)
-		mu.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		kuufsIPs, _ := scanMonitorManagers(operatingRoom)
-		mu.Lock()
-		allReachableIPs = append(allReachableIPs, kuufsIPs...)
-		mu.Unlock()
-	}()
-
-	wg.Wait()
-	return allReachableIPs, nil
-}
-
-func scanGERSManagers(operatingRoom string) ([]string, error) {
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	var reachableIPs []string
-
-	for _, ip := range obtainPossibleIPsForGERSManager(operatingRoom) {
-		wg.Add(1)
-
-		go func(ip string) {
-			defer wg.Done()
-			success, _, err := Ping(ip, 80)
-			if err == nil && success {
-				mu.Lock()
-				reachableIPs = append(reachableIPs, ip)
-				mu.Unlock()
-			}
-		}(ip)
-	}
-
-	wg.Wait()
-	return reachableIPs, nil
-}
-
-func scanMonitorManagers(operatingRoom string) ([]string, error) {
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	var reachableIPs []string
-
-	for _, ip := range obtainPossibleIPsForMonitorManager(operatingRoom) {
-		wg.Add(1)
-
-		go func(ip string) {
-			defer wg.Done()
-			success, _, err := Ping(ip, 80)
-			if err == nil && success {
-				mu.Lock()
-				reachableIPs = append(reachableIPs, ip)
-				mu.Unlock()
-			}
-		}(ip)
-	}
-
-	wg.Wait()
-	return reachableIPs, nil
-}
-
-func Ping(address string, port int) (bool, time.Duration, error) {
-	startTime := time.Now()
-
-	addr := fmt.Sprintf("%s:%d", address, port)
-
-	conn, err := net.DialTimeout("tcp", addr, 2*time.Second)
-	if err != nil {
-		return false, 0, fmt.Errorf("failed to connect: %v", err)
-	}
-	defer conn.Close()
-
-	duration := time.Since(startTime)
-
-	return true, duration, nil
 }
